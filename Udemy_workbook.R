9953399ad165fd0904ec4409ebfa5d4a75ed9777
doctorhh <hans.harvey@h2hteam.com>
2016-08-08 18:35:13 +0200

    First draft
    and working script

---
+++ Udemy_workbook.R
@@ -0,0 +1,21 @@
+myfirstfn <- function(x){
+  x+x
+}
+
+mysecondfn <- function(t,z){
+  value <- z*3
+  value <- value * t
+  print(value)
+}
+for (i in 1:15){print(i)}
+
+PrimVec <- function(n){
+  
+  if (n>2){
+    s <-seq(2,n)
+    p<- c()
+  }
+}
+x<-5:7
+y<-8:10
+plot(x,y)
